fd65940f6c9cb1ca5a65751586859047d2c17ed2
Nobuyoshi Meki <mekinobuyoshi@gmail.com>
2026-01-04 14:08:34 +0900

    01041408

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/**/*.{js,ts,jsx,tsx,mdx}", // これがあれば src 以下の全ファイルカバーできる！
+    "./src/**/*.{js,ts,jsx,tsx,mdx}", 
   ],
 theme: {
     container: {
       center: true,
       padding: '1rem',
-      // これを足すと、1280pxで幅が止まって、それ以上は左右の余白になる
+   
       screens: {
         sm: '640px',
         md: '768px',
         lg: '1024px',
         xl: '1280px',
-        '2xl': '1280px', // ここがポイント！2xlになっても1280pxのままにする
+        '2xl': '1280px', 
       },
     },
     extend: {},
